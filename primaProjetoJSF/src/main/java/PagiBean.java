958f4e036f0481ebb61465caf62f36ca578ffa79
Xavierzn <102821876+Xavierzn@users.noreply.github.com>
2022-04-19 15:44:10 -0300

    Add files via upload

---
+++ primaProjetoJSF/src/main/java/PagiBean.java
@@ -0,0 +1,14 @@
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@SuppressWarnings("serial")
+@Named
+@ApplicationScoped
+
+public class PagiBean implements  Serializable {
+	
+	
+	
+}
