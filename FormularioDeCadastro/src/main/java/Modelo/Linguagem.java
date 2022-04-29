b61dbbc8eae154f3595c59f197b0619e2d011892
Xavierzn <102821876+Xavierzn@users.noreply.github.com>
2022-04-29 15:40:33 -0300

    Add files via upload

---
+++ FormularioDeCadastro/src/main/java/Modelo/Linguagem.java
@@ -0,0 +1,60 @@
+package Modelo;
+
+public class Linguagem {
+	
+	private Integer id;
+	private String nome;
+	
+	public static final Linguagem[] LINGUAGENS; // o final ele cria um constante, quando se cria constante, o nome da constante deve ser com letrasMaiusculas
+	
+		static { // o static serve para inicializar o array de constantes
+			
+			LINGUAGENS = new Linguagem[5];
+			
+			LINGUAGENS[0] = new Linguagem(1, "Java");
+			LINGUAGENS[1] = new Linguagem(2, "IOS");
+			LINGUAGENS[2] = new Linguagem(3, "C++");
+			LINGUAGENS[3] = new Linguagem(4, "C#");
+			LINGUAGENS[4] = new Linguagem(5, "PHP");
+			
+		}
+		
+		
+	
+
+	public Linguagem(Integer id, String nome) { // ele cria automaticamente a  new Linguagem
+	
+		this.id = id;
+		this.nome = nome;
+	}
+
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
---
+++ FormularioDeCadastro/src/main/java/indexBean.java
@@ -0,0 +1,7 @@
+import javax.enterprise.context.RequestScoped;
+
+@RequestScoped
+
+public class indexBean {
+
+}
